11c8649ea7db9e7886af0779a282060d68d6215a
Sandeep Nayak <C123267@exelonds.com>
2019-04-24 12:19:51 +0530

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 var httpService = require('./request-handler');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(httpService.handleRequest);
